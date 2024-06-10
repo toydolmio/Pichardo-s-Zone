010d70fec3ca9b7fec1b958626099b6ea842dd48
toydolmio <warnersilverio5@gmail.com>
2024-06-10 12:25:50 -0400

    Add files via upload

---
+++ Exercise 3/Exercise 3.13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   int I, PRI = 0, SEG = 1, SIG;
+   printf("\t %d \t %d", PRI, SEG);
+
+   for (I = 3; I <= 50; I++)
+   {
+       SIG = PRI + SEG;
+       PRI = SEG;
+       SEG = SIG;
+       printf("\t %d", SIG);
+   }
+}
